071996917f1178d64c6213ab1a571c0b2007739c
amaliafitrii <143860240+amaliafitrii@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    Add files via upload
    upload file

---
+++ amalia.py
@@ -0,0 +1,31 @@
+nama_saya =str(input("masukkan nama kamu"))
+print(nama_saya, "bertipe", type(nama_saya))
+umur = int(input("masukkan umur kamu = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukkan IPK kamu = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("sudah menikah? = "))
+print("menikah?",menikah,"bertipe", type(menikah))
+
+matkul_diambil - ["alpro", "sig"]
+print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+matkul_diambil2 - ("web", "pemvis")
+print("Matkul : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 - {"Alpro", "Alpro", "PemWeb", "Data Science", "Data Science"}
+print("Matkul Lain-Lain: ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+
+
+
+
+
+print()
+print()
+
+print(17 / 3)
+print(17 // 3)
+print(37 % 3)
